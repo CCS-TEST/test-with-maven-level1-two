85b1f6ce651ab60fd30e2cef341d0b7137f55d6c
Pratyush Giri <pratyush.g@sparkcentral.com>
2018-07-25 18:41:07 -0700

    added new tests.

--- src/test/java/ccs/test/mvn/level1/two/TestEndy.java
+++ src/test/java/ccs/test/mvn/level1/two/TestEndy.java
@@ -29,12 +29,36 @@ public class TestEndy {
 
     @Test
     public void testHappy1() {
-
         int[] returned =  endy.copyEndy(new int[]{9, 11, 90, 22, 6}, 2);
         assertNotNull(returned);
         assertEquals(2, returned.length);
         assertEquals(9, returned[0] );
         assertEquals(90, returned[1] );
     }
-    
+
+    @Test
+    public void testHappy2() {
+        int[] returned =  endy.copyEndy(new int[]{9, 11, 90, 22, 6}, 3);
+        assertNotNull(returned);
+        assertEquals(3, returned.length);
+        assertEquals(9, returned[0] );
+        assertEquals(90, returned[1] );
+        assertEquals(6, returned[2] );
+    }
+
+    @Test
+    public void testHappy3() {
+        int[] returned =  endy.copyEndy(new int[]{0}, 1);
+        assertNotNull(returned);
+        assertEquals(1, returned.length);
+        assertEquals(0, returned[0] );
+    }
+    @Test
+    public void testHappy4() {
+        int[] returned =  endy.copyEndy(new int[]{13, 2, 2, 0, 2}, 2);
+        assertNotNull(returned);
+        assertEquals(2, returned.length);
+        assertEquals(2, returned[0] );
+        assertEquals(2, returned[1] );
+    }
 }
